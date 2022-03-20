0002d7451ca34e930382d5c02c16a6831e2a3fc6
Kyle Guthery <ksguthery@gmail.com>
2022-03-20 09:41:22 -0500

    finished i think

---
+++ stock-picker.rb
@@ -0,0 +1,21 @@
+def stock_picker(stocks)
+  biggest_change = 0
+  new_arr = Array.new
+  stocks.each_index do |index|
+    stocks.each_index do |comparison|
+      if comparison > index
+        change = stocks[comparison] - stocks[index]
+        if change > biggest_change
+          biggest_change = change
+          new_arr = [index, comparison]
+        end
+        # p "#{stocks[index]} change #{change}"
+      end
+    end
+  end
+  p "Best change is #{biggest_change}"
+  p "New array is #{new_arr}"
+  return new_arr
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
